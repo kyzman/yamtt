7d6697202200e13c461ab498929b40afa06d9e1a
Andrew <kyzwork@yandex.ru>
2024-04-10 15:46:12 +0500

    Create Model and test constraint

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(models.Q(parent__isnull=True) & models.Q(
+                    title__regex=r"[A-Za-z_0-9]+")) | models.Q(
+                    parent__isnull=False), name='CK_name_parent')
+        ]
+
+    def __str__(self):
+        return f"[{self.pk}] {self.title}"
--- menu/tests.py
+++ menu/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from menu.models import Menu
+from django.db.utils import IntegrityError
 
 # Create your tests here.
+
+
+class TestMenuModel(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Menu.objects.create(title='some_menu')
+
+    def test_valid_constraint(self):
+        menu = Menu.objects.get(title='some_menu')
+        self.assertIsNotNone(menu)
+
+    def test_invalid_cyr_constraint(self):
+        with self.assertRaises(IntegrityError):
+            new = Menu.objects.create(title='Меню')
+
+    def test_valid_cyr_constraint(self):
+        menu = Menu.objects.get(title='some_menu')
+        new_item = Menu.objects.create(title='Меню', parent=menu)
+        self.assertIsNotNone(new_item)
