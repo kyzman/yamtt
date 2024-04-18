e4caf3e00ceca2dc786b31343dd21d3776c26519
Andrew <kyzwork@yandex.ru>
2024-04-18 18:55:24 +0500

    Move url formation from template to code. Add url fragments support

--- menu/templatetags/draw_menu.py
+++ menu/templatetags/draw_menu.py
@@ -60,12 +60,15 @@ def get_all_items_by_menu(menu):
     count = 0
     for item in all_items:
         result.append(model_to_dict(item))
-        parted = str.partition(item.url, '?')
+        print(item.url)
+        fragmented = str.partition(item.url, '#')
+        parted = str.partition(fragmented[0], '?')
         try:
             absolute_url = f"{reverse(parted[0])}{parted[1]}{parted[2]}"
         except NoReverseMatch:  # если не получилось получить абсолютный url по полю, значит считаем что указан прямой.
-            absolute_url = item.url
-        result[count]['url'] = absolute_url
+            absolute_url = fragmented[0]
+        result[count]['url'] = f"{absolute_url}&{menu}={item.id}" if '?' in absolute_url else f"{absolute_url}?{menu}={item.id}"
+        result[count]['url'] += f"{fragmented[1]}{fragmented[2]}"
         count += 1
     return result
 
